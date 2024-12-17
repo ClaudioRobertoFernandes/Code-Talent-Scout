b4cca0018dc8babdd7e475c10a56fe811dd6c065
claudio Roberto Fernandes <claudio.crfdev@gmail.com>
2024-12-16 23:41:51 -0300

    Add new component menu-item-dark-mode

---
+++ resources/views/components/menu-item-dark-mode.blade.php
@@ -0,0 +1,17 @@
+<div class="menu-item mb-0.5">
+    <div class="menu-link">
+        <span class="menu-icon">
+            <i class="ki-filled ki-moon"></i>
+        </span>
+        <span class="menu-title">
+            {{ __('Dark Mode') }}
+        </span>
+        <label class="switch switch-sm">
+            <input
+                data-theme-state="dark"
+                data-theme-toggle="true"
+                name="check"
+                type="checkbox" value="1"/>
+        </label>
+    </div>
+</div>
